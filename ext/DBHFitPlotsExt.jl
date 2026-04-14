2ab0daa65b3bdae29bda53e1dfa04a3495f76b25
hunterbuffer11 <1667365267@qq.com>
2026-04-14 12:06:49 +0800

    Ext_pull

--- ext/DBHFitPlotsExt.jl
+++ ext/DBHFitPlotsExt.jl
@@ -1,5 +1,4 @@
-# DBHFit Plots扩展
-# 当加载Plots.jl时自动启用可视化功能
+# DBHFit Plots Ext
 
 module DBHFitPlotsExt
 
@@ -9,7 +8,8 @@ using Plots
 """
     DBHFit.plot_fit(x, y, result; kwargs...)
 
-绘制拟合结果可视化图形。
+Plot visualization graphs of the fitting results
+
 """
 function DBHFit.plot_fit(x::AbstractVector{T}, y::AbstractVector{T}, 
                          result::DBHFit.CircleFitResult; 
@@ -17,7 +17,8 @@ function DBHFit.plot_fit(x::AbstractVector{T}, y::AbstractVector{T},
                          markersize::Real=1,
                          aspect_ratio::Symbol=:equal,
                          kwargs...) where T<:Real
-    # 创建散点图
+
+    # Scattering plot of points
     p = Plots.scatter(x, y; 
                       size=size, 
                       grid=true,
@@ -27,13 +28,13 @@ function DBHFit.plot_fit(x::AbstractVector{T}, y::AbstractVector{T},
                       aspect_ratio=aspect_ratio,
                       kwargs...)
     
-    # 绘制拟合圆
+    # Calculate the circle based on the fitting result
     θ = LinRange(0, 2π, 100)
     circle_x = result.center_x .+ result.radius * cos.(θ)
     circle_y = result.center_y .+ result.radius * sin.(θ)
     Plots.plot!(p, circle_x, circle_y; linewidth=3, label="Fitted Circle", color=:red)
     
-    # 标记圆心
+    # Plot the center of the circle
     Plots.scatter!(p, [result.center_x], [result.center_y]; 
                    label="Center",
                    markershape=:circle,
@@ -41,7 +42,7 @@ function DBHFit.plot_fit(x::AbstractVector{T}, y::AbstractVector{T},
                    markercolor=:blue,
                    markerstrokecolor=:white)
     
-    # 绘制半径线
+    # Plot the radius
     x_rim = result.center_x + result.radius
     y_rim = result.center_y
     Plots.plot!(p, [result.center_x, x_rim], [result.center_y, y_rim];
@@ -50,7 +51,7 @@ function DBHFit.plot_fit(x::AbstractVector{T}, y::AbstractVector{T},
                 linecolor=:red,
                 label="Radius")
     
-    # 添加半径标注
+    # Add radius annotation
     mid_x = (result.center_x + x_rim) / 2
     mid_y = result.center_y + 0.015
     Plots.annotate!(p, mid_x, mid_y, 
@@ -60,4 +61,4 @@ function DBHFit.plot_fit(x::AbstractVector{T}, y::AbstractVector{T},
     return p
 end
 
-end # module
+end 
--- ext/DBHFitPointCloudUtilsExt.jl
+++ ext/DBHFitPointCloudUtilsExt.jl
@@ -1,5 +1,4 @@
-# DBHFit PointCloudUtils扩展
-# 当加载PointCloudUtils时自动启用点云处理功能
+# DBHFit PointCloudUtils Ext
 
 module DBHFitPointCloudUtilsExt
 
@@ -9,17 +8,16 @@ using PointCloudUtils
 """
     DBHFit.fit_dbh(pc::PointCloud; kwargs...)
 
-直接处理PointCloud类型的点云数据。
+Directly process point cloud data of the PointCloud type.
 """
 function DBHFit.fit_dbh(pc::PointCloud{T}; 
                         height_range::Union{Tuple{Real,Real},Nothing}=nothing,
                         kwargs...) where T<:Real
-    # 如果指定了高度范围，先提取DBH区域
+    # DBH range
     if height_range !== nothing
         pc = PointCloudUtils.extract_dbh(pc, height_range[1], height_range[2])
     end
-    
-    # 调用基础拟合函数
+    # Call the base fitting function
     return DBHFit.fit_dbh(pc.x, pc.y; kwargs...)
 end
 
