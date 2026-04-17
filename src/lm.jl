919a56c85eab3a050c77d87dfeb3df35b00342c6
hunterbuffer11 <1667365267@qq.com>
2026-04-17 16:24:27 +0800

    LM

--- src/lm.jl
+++ src/lm.jl
@@ -70,7 +70,7 @@ Fit a circle using Levenberg-Marquardt algorithm via LsqFit.jl.
 function fit_circle_lm(x::AbstractVector{T}, y::AbstractVector{T}; 
                        max_iter::Int=50,  
                        robust::Bool=false,
-                       tukey_threshold::Real=1.685,
+                       tukey_threshold::Real=4.685,
                        kwargs...) where T<:Real
     n = length(x)
     
@@ -85,13 +85,13 @@ function fit_circle_lm(x::AbstractVector{T}, y::AbstractVector{T};
         w = ones(T, n)
         for iter in 1:max_iter
             fit = curve_fit(circle_model, circle_jacobian, xdata, ydata, w, p0;
-                           maxIter=max_iter)
+                           maxIter=1)
             p0 = fit.param
             
             residuals = fit.resid
             r_median = median(abs.(residuals))
             mad_val = max(median(abs.(residuals .- r_median)), 1e-8)
-            
+
             if mad_val < 1e-10
                 break
             end
